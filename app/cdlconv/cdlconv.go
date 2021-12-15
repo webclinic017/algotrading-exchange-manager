4932d8cb63c561d40de6eec2fe089c623f94a557
parag-b <parag+github@barangale.in>
2021-12-15 08:05:51 +0530

    cdl logger removed closes #20

--- app/cdlconv/cdlconv.go
+++ app/cdlconv/cdlconv.go
@@ -1,13 +1,13 @@
 package cdlconv
 
 func Convert1MinCandle() {
-	println("in 1 min candle")
+	// println("in 1 min candle")
 }
 
 func Convert3MinCandle() {
-	println("in 3 min candle")
+	// println("in 3 min candle")
 }
 
 func Convert5MinCandle() {
-	println("in 5 min candle")
+	// println("in 5 min candle")
 }
